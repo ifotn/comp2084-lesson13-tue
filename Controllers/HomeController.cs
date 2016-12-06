31eabbf1f0f7ae36ede7542f65c82ec5356d1224
ifotn <rich@infrontofthenet.com>
2016-12-06 08:25:33 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Week13.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Location()
+        {
+            // set a message to be displayed in the view
+            ViewBag.Message = "We will show a map here soon.";
+
+            // load the new view
+            return View();
+            //return View("~/Views/Home/artem.cshtml");
+        }
+    }
+}
---
+++ Controllers/StoreController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Week13.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public ActionResult Index()
+        {
+            // connect to the db
+            using (Models.DefaultConnection db = new Models.DefaultConnection())
+            {
+                // use the Genre model to get all the genres
+                var genres = db.Genres.ToList();
+
+                // create a list sorted by Name and pass that instead of the original list
+                var genresAz = from g in genres
+                               orderby g.Name ascending
+                               select g;
+
+                // load the view and pass it the query results
+                return View(genresAz);
+            }
+
+        }
+
+        // GET: /Store/Browse
+        public ActionResult Browse(string genre)
+        {
+            //try { 
+            // connect to the db and query for the right data
+            using (Models.DefaultConnection db = new Models.DefaultConnection())
+            {
+                // get the selected genre and the related albums
+                var genreData = db.Genres.Include("Albums")
+                    .SingleOrDefault(g => g.Name == genre);
+
+                if (genreData == null)
+                {
+                    return RedirectToAction("Index");
+                    //return View("Index");
+                }
+                else
+                {
+                    // load the view
+                    return View(genreData);
+                }
+            }
+
+        }
+
+        // GET: /Store/Details
+        public String Details(int id)
+        {
+            string message = "Store.Details, ID = " + id;
+            return message;
+        }
+    }
+}
